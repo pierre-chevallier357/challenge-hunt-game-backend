338240c76b7d55190da3e8ff4e59a92d0d21b494
Léo <contact.leeo97one@gmail.com>
2021-04-13 13:13:20 +0200

    Version 0 : User

--- src/main/java/com/example/User.java
+++ src/main/java/com/example/User.java
@@ -1,5 +1,27 @@
 package com.example;
 
 public class User {
-    public String description;
+    private String login;
+    private int age;
+
+    public User(String login, int age) {
+        this.login = login;
+        this.age = age;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
---
+++ src/main/java/com/example/UserCRUD.java
@@ -0,0 +1,129 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/users")
+public class UserCRUD {
+	@Autowired
+	private DataSource dataSource;
+
+	@GetMapping("/")
+	public ArrayList<User> allUsers(HttpServletResponse response) {
+		ArrayList<User> users = new ArrayList<>();
+
+		try {
+			PreparedStatement statement = dataSource.getConnection().prepareStatement(
+					"SELECT login, age FROM chamis"
+			);
+
+			ResultSet r = statement.executeQuery();
+			while (r.next()) {
+				User user = new User(
+						r.getString("login"),
+						r.getInt("age")
+				);
+				users.add(user);
+			}
+		} catch (SQLException throwables) {
+			throwables.printStackTrace();
+		}
+
+		return users;
+	}
+
+	@GetMapping("/{userId}")
+	public User read(@PathVariable(value = "userId") String id, HttpServletResponse response) {
+		User user = null;
+		try {
+			PreparedStatement statement = dataSource.getConnection().prepareStatement(
+					"SELECT login, age FROM chamis WHERE login = ?"
+			);
+			statement.setString(1,id);
+
+			ResultSet r = statement.executeQuery();
+			if (r.next()) {
+				user = new User(
+						r.getString(1),
+						r.getInt(2)
+				);
+			} else {
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+			}
+		} catch (SQLException throwables) {
+			throwables.printStackTrace();
+		}
+
+		return user;
+	}
+
+	@PostMapping("/{userId}")
+	public User create(@PathVariable(value = "userId") String id, @RequestBody User u, HttpServletResponse response) {
+		if (!u.getLogin().equals(id)) {
+			throw new ResponseStatusException(HttpStatus.PRECONDITION_FAILED);
+		}
+
+		try {
+			PreparedStatement statement = dataSource.getConnection().prepareStatement(
+					"INSERT INTO chamis (login, age) VALUES (?, ?)"
+			);
+			statement.setString(1, u.getLogin());
+			statement.setInt(2, u.getAge());
+
+			statement.executeUpdate();
+		} catch (SQLException throwables) {
+			throwables.printStackTrace();
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+		}
+
+		return u;
+	}
+
+	@PutMapping("/{userId}")
+	User update(@PathVariable(value = "userId") String id, @RequestBody User u, HttpServletResponse response) {
+		if (!u.getLogin().equals(id)) {
+			throw new ResponseStatusException(HttpStatus.PRECONDITION_FAILED);
+		}
+
+		try {
+			PreparedStatement statement = dataSource.getConnection().prepareStatement(
+					"UPDATE chamis SET login = ?, age = ? WHERE login = ?"
+			);
+			statement.setString(1, u.getLogin());
+			statement.setInt(2, u.getAge());
+			statement.setString(3, id);
+
+			statement.executeUpdate();
+		} catch (SQLException throwables) {
+			throwables.printStackTrace();
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+		return u;
+	}
+
+	@DeleteMapping("/{userId}")
+	void delete(@PathVariable(value = "userId") String id, HttpServletResponse response) {
+		try {
+			PreparedStatement statement = dataSource.getConnection().prepareStatement(
+					"DELETE FROM chamis WHERE login = ?"
+			);
+			statement.setString(1, id);
+
+			statement.executeUpdate();
+		} catch (SQLException throwables) {
+			throwables.printStackTrace();
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+	}
+}
