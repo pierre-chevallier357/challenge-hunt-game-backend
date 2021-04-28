00e3e6dc16e346d4f997e0bf35cac64a69cb00cb
Léo <contact.leeo97one@gmail.com>
2021-04-28 11:05:44 +0200

    Chami/Defi : get defis by uid Fixes #3

--- src/main/java/com/example/crudrest/ChamiCrud.java
+++ src/main/java/com/example/crudrest/ChamiCrud.java
@@ -1,16 +1,26 @@
 package com.example.crudrest;
 
 import com.example.entity.Chami;
+import com.example.entity.Defi;
 import com.example.repository.ChamiRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.repository.DefiRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api/chamis")
 public class ChamiCrud extends CrudRest<Chami, Integer, ChamiRepository> {
-	public ChamiCrud(ChamiRepository repository) {
+	private final DefiRepository defiRepository;
+
+	public ChamiCrud(ChamiRepository repository, DefiRepository defiRepository) {
 		super(repository);
+		this.defiRepository = defiRepository;
+	}
+
+	@GetMapping("/{id}/defis")
+	public List<Defi> readDefis(@PathVariable(value = "id") final int id) {
+		return defiRepository.findByUid(id);
 	}
 }
--- src/main/java/com/example/repository/DefiRepository.java
+++ src/main/java/com/example/repository/DefiRepository.java
@@ -3,5 +3,8 @@ package com.example.repository;
 import com.example.entity.Defi;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface DefiRepository extends CrudRepository<Defi, String>, WithInsert<Defi> {
+	public List<Defi> findByUid(int uid);
 }
