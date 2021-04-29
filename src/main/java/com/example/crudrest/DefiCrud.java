363238cde3fdad9a47f05788384a10f91e424406
l3miage-kailir <77158282+l3miage-kailir@users.noreply.github.com>
2021-04-29 16:31:54 +0200

    Ajout de VisiteByIdDefi côté serveur

--- src/main/java/com/example/crudrest/DefiCrud.java
+++ src/main/java/com/example/crudrest/DefiCrud.java
@@ -5,12 +5,21 @@ import com.example.repository.DefiRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.repository.VisiteRepository;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api/defis")
 public class DefiCrud extends CrudRest<Defi, String, DefiRepository> {
-	public DefiCrud(DefiRepository repository) {
+	private final VisiteRepository visiteRepository;
+
+	public DefiCrud(DefiRepository repository, VisiteRepository visiteRepository) {
 		super(repository);
+		this.visiteRepository = visiteRepository;
+	}
+
+	@GetMapping("/{idDefi}/visites")
+	public List<Visite> readVisites(@PathVariable(value = "idDefi") final String idDefi) {
+		return visiteRepository.findByIdDefi(idDefi);
 	}
 }
--- src/main/java/com/example/repository/VisiteRepository.java
+++ src/main/java/com/example/repository/VisiteRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface VisiteRepository extends CrudRepository<Visite, String>, WithInsert<Visite> {
 	List<Visite> findByUid(int uid);
+	List<Visite> findByIdDefi(String idDefi);
 }
