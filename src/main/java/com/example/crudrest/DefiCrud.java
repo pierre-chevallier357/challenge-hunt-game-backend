064dec6ccc1b1421f4ec0514dc64a170f8c02d7d
l3miage-kailir <77158282+l3miage-kailir@users.noreply.github.com>
2021-04-30 17:20:30 +0200

    Ajout de: getquestionbyiddefi

--- src/main/java/com/example/crudrest/DefiCrud.java
+++ src/main/java/com/example/crudrest/DefiCrud.java
@@ -3,8 +3,11 @@ package com.example.crudrest;
 import java.util.List;
 
 import com.example.entity.Defi;
+import com.example.entity.Question;
 import com.example.entity.Visite;
 import com.example.repository.DefiRepository;
+import com.example.repository.QuestionRepository;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,14 +20,21 @@ import com.example.repository.VisiteRepository;
 @RequestMapping("/api/defis")
 public class DefiCrud extends CrudRest<Defi, String, DefiRepository> {
 	private final VisiteRepository visiteRepository;
+	private QuestionRepository questionRepository;
 
-	public DefiCrud(DefiRepository repository, VisiteRepository visiteRepository) {
+	public DefiCrud(DefiRepository repository, VisiteRepository visiteRepository, QuestionRepository questionRepository) {
 		super(repository);
 		this.visiteRepository = visiteRepository;
+		this.questionRepository = questionRepository;
 	}
 
 	@GetMapping("/{idDefi}/visites")
 	public List<Visite> readVisites(@PathVariable(value = "idDefi") final String idDefi) {
 		return visiteRepository.findByIdDefi(idDefi);
+	}
+
+	@GetMapping("/{idDefi}/questions")
+	public List<Question> readQuestion(@PathVariable(value = "idDefi") final String idDefi) {
+		return questionRepository.findByIdDefi(idDefi);
 	}
 }
--- src/main/java/com/example/repository/QuestionRepository.java
+++ src/main/java/com/example/repository/QuestionRepository.java
@@ -1,7 +1,10 @@
 package com.example.repository;
 
+import java.util.List;
+
 import com.example.entity.Question;
 import org.springframework.data.repository.CrudRepository;
 
 public interface QuestionRepository extends CrudRepository<Question, Integer>, WithInsert<Question> {
+    List<Question> findByIdDefi(String idDefi);
 }
